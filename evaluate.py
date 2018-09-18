940f007fdeeb1782b154b70f928d6e35e5ca9594
aiinside-6 <tripdancer0916@gmail.com>
2018-09-18 12:29:01 +0900

    evaluate trained model

---
+++ evaluate.py
@@ -0,0 +1,41 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import keras
+from keras import optimizers
+from keras import backend as K
+from keras.losses import categorical_crossentropy as logloss
+from keras.metrics import categorical_accuracy, top_k_categorical_accuracy
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.engine.topology import Input, Container
+from keras.engine.training import Model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, advanced_activations, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D, Convolution2D, pooling, Lambda, concatenate
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.models import load_model as keras_load_model
+import os
+
+num_classes = 10
+# The data, split between train and test sets:
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+model = keras_load_model('cifar10vgg.h5')
+
+# Score trained model.
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
+
---
+++ make_soft_target.py
@@ -0,0 +1,39 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import numpy as np
+
+import keras
+from keras import optimizers
+from keras import backend as K
+from keras.losses import categorical_crossentropy as logloss
+from keras.metrics import categorical_accuracy, top_k_categorical_accuracy
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.engine.topology import Input, Container
+from keras.engine.training import Model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, advanced_activations, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D, Convolution2D, pooling, Lambda, concatenate
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.models import load_model as keras_load_model
+import os
+
+model = keras_load_model('teachermodel.ep11.h5')
+
+batch_size = 100
+num_classes = 10
+epochs = 300
+num_predictions = 20
+
+# The data, split between train and test sets:
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print('y_train shape:', y_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+z = model.predict(x_test)
