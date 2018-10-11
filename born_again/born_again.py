193970ace4710a12847806c3212a5534b3b10b2b
tripdancer0916@gmail.com <ritalin202>
2018-10-11 20:26:08 +0900

    tmp

--- born_again/born_again.py
+++ born_again/born_again.py
@@ -164,7 +164,7 @@ class MyIterator(object):
 
     def __next__(self):
         tmp = next(self.iterator)
-        return [tmp[0], tmp[1]], tmp[1]
+        return tmp, tmp[1]
 
 
 if __name__ == '__main__':
